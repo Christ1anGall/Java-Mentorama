6786310c5b44f750c71e1d1bfcfa4990a0eaf4c7
Christian Gall <christiangall92@icloud.com>
2023-05-13 13:26:53 -0300

    wrapper

---
+++ Basico/M5 - Numeros Linhas e Datas/Wrapper.java
@@ -0,0 +1,14 @@
+public class Wrapper {
+    public static void main(String[] args) {
+        // int
+        // byte
+        // char
+        // boolean
+
+        Integer myInt = Integer.valueOf(42); // Cria uma referência de wrapper para um int com valor 42
+        Byte myByte = Byte.valueOf((byte) 123); // Cria uma referência de wrapper para um byte com valor 123
+        Character myChar = Character.valueOf('x'); // Cria uma referência de wrapper para um char com valor 'x'
+        Boolean myBoolean = Boolean.valueOf(true); // Cria uma referência de wrapper para um boolean com valor true
+
+    }
+}
