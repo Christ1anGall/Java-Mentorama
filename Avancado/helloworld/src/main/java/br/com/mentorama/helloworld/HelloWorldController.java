932587c5f0c07959d27d7031d5ae6f4cc40d24cf
Christian Gall <christiangall92@icloud.com>
2023-05-31 09:11:54 -0300

    Message ArrayList

--- Avancado/helloworld/src/main/java/br/com/mentorama/helloworld/HelloWorldController.java
+++ Avancado/helloworld/src/main/java/br/com/mentorama/helloworld/HelloWorldController.java
@@ -1,5 +1,8 @@
 package br.com.mentorama.helloworld;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,11 +10,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/HelloWorld")
 public class HelloWorldController {
-    private final String message = "Hello World Batata";
+    private final String message = "Hello World";
+    private final List<Message> messages;
+
+    public HelloWorldController() {
+        this.messages = new ArrayList<>();
+
+        Message message2 = new Message(1, message);
+        Message message3 = new Message(2, message);
+        Message message4 = new Message(3, message);
+
+        this.messages.add(message2);
+        this.messages.add(message3);
+        this.messages.add(message4);
+    }
 
     @GetMapping
-    public String HelloWorld() {
-        return message;
+    public List<Message> HelloWorld() {
+        return messages;
     }
 
 }
